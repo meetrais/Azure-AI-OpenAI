980c62c43725e62540a2bd7088436754760ff8f0
Rais Kazi <meetrais@gmail.com>
2024-04-14 17:45:57 -0500

    Dot-Net: Added code for Chat with your data.

--- Dot-Net/azureopenaiexample/Controllers/ChatController.cs
+++ Dot-Net/azureopenaiexample/Controllers/ChatController.cs
@@ -5,6 +5,10 @@ using Azure.AI.OpenAI;
 using Azure;
 using DotNetEnv;
 using static System.Environment;
+using iText.Kernel.Pdf;
+using System.Text;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
 
 
 namespace azureopenaiexample.Controllers;
@@ -25,7 +29,7 @@ public class ChatController : Controller
         return View();
     }
 
-    [HttpPost]
+    [HttpGet]
     public async Task<IActionResult> GetChatResponse(string userQuestion)
     {
         try
@@ -38,14 +42,15 @@ public class ChatController : Controller
             var chatCompletionOptions = new ChatCompletionsOptions(){
                 DeploymentName=model,
                 Messages={
-                    new ChatRequestSystemMessage("Helpful a helpful AI assistant."),
+                    new ChatRequestSystemMessage("You are a helpful AI assistant."),
                     /// Single-shot Prompt
                     new ChatRequestUserMessage("Is Washington DC Capital of USA?"),
                     new ChatRequestAssistantMessage("Yes, Washington DC is the Capital of USA"),
                     /////////////
                     new ChatRequestUserMessage(userQuestion)
             },
-            MaxTokens=500
+            MaxTokens=500,
+            Temperature=2 //Max Creativity
             };
 
             Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionOptions);
@@ -57,8 +62,68 @@ public class ChatController : Controller
         catch(Exception ex){
             return Json(new {Response=ex.Message});
         }
-
-        
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetChatWithYourDataResponse(string userQuestion)
+    {
+        try
+        {
+            string endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string key = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY");
+            string model = Environment.GetEnvironmentVariable("AZURE_OPENAI_MODEL");
+
+            OpenAIClient client = new OpenAIClient(new Uri(endpoint), new Azure.AzureKeyCredential(key));
+            string pdfText = GetTextFromPDF(@"YOUR_PDF_FILE_PATH");
+            if(pdfText.Length>8192)
+                pdfText = pdfText.Substring(0,8192);//I am using gpt-35-turbo which has max context length of 8192.
+            var chatCompletionOptions = new ChatCompletionsOptions(){
+                DeploymentName=model,
+                Messages={
+                    new ChatRequestSystemMessage("You are a helpful AI assistant."),
+                    new ChatRequestUserMessage("Information from your PDF: " + pdfText),
+                    new ChatRequestUserMessage(userQuestion)
+            },
+            MaxTokens=500,
+            Temperature=0//No Creativity, more grounded response
+            };
+
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionOptions);
+
+            var result = response.Value.Choices.First().Message.Content;
+
+            return Json(new {Response=result});
+        }
+        catch(Exception ex){
+            return Json(new {Response=ex.Message});
+        }
+    }
+
+    private  string GetTextFromPDF(string filePath)
+    {
+        try
+        {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(filePath));
+            StringBuilder text = new StringBuilder();
+
+            for(int i=1; i<=pdfDocument.GetNumberOfPages(); i++)
+            {
+                PdfPage pdfPage= pdfDocument.GetPage(i);
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string currentPageText = PdfTextExtractor.GetTextFromPage(pdfPage, strategy);
+                text.Append(currentPageText);
+            }
+
+            pdfDocument.Close();
+            return text.ToString();
+
+        }
+        catch(Exception ex)
+        {
+        return ex.Message;
+        }
+     }
+
+
 
 }
--- Dot-Net/azureopenaiexample/obj/Debug/net7.0/azureopenaiexample.AssemblyInfo.cs
+++ Dot-Net/azureopenaiexample/obj/Debug/net7.0/azureopenaiexample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azureopenaiexample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccf61bb7ae962ac9e639d41fd3c193614290399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2aa14cbcf281188a099563e6cef55f313c371f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("azureopenaiexample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azureopenaiexample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
